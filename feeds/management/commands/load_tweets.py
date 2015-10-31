241fec8c385952ed23e57e80aec038f0f29f661d
aloktripathy <alokrant@gmail.com>
2015-10-31 06:21:13 +0530

    Changed Tweet Document structure

--- feeds/management/commands/load_tweets.py
+++ feeds/management/commands/load_tweets.py
@@ -7,6 +7,9 @@ from feeds.models import Category
 class Command(BaseCommand):
     help = 'Schedules task routines'
 
+    # cron
+    # */15 * * * * /root/.virtualenvs/twn-env/bin/python3 manage.py load_tweets
+
     def handle(self, *args, **kwargs):
         Category.refresh_all(50)
         self.stdout.write('Tweets loaded successfully')
